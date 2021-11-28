b53319a041fc49458649701c97a825367b21d496
Maxim Giryaev <gemtmp@yandex-team.ru>
2021-11-28 22:41:40 +0300

    add common project

---
+++ common/button.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Arduino.h"
+
+namespace gemha {
+
+class Button {
+public:
+	Button(uint8_t pin) : pin(pin) {
+		pinMode(pin, INPUT_PULLUP);
+	}
+
+	void check() {
+		bool v = digitalRead(pin);
+		if (v == val) {
+			counter = 0;
+			return;
+		}
+		counter++;
+		if (counter >= delay) {
+			val = v;
+		}
+	}
+	operator bool() const {
+		return val;
+	}
+	bool value() const {
+		return val;
+	}
+private:
+	const uint8_t pin;
+	bool val = false; // TODO volatile ?
+	uint8_t counter = 0;
+	const static uint8_t delay = 5;
+};
+
+} // namespace gemha
---
+++ common/temperature.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "Arduino.h"
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <PubSubClient.h>
+
+namespace gemha {
+
+
+class Temperature {
+public:
+	Temperature(const char* topic, OneWire* oneWire, PubSubClient* client) :
+			topic(topic), oneWire(oneWire), sensors(oneWire), client(client) {
+		topicLen = strlen(topic);
+	}
+
+	void start() {
+		sensors.begin();
+	}
+
+	void read() {
+		oneWire->reset_search();
+
+		addressCount = 0;
+		while (addressCount < ADDRESS_MAX && oneWire->search(devices[addressCount].addr)) {
+			DeviceAddress &addr = devices[addressCount].addr;
+			if (sensors.validAddress(addr)) {
+				if (sensors.validFamily(addr)) {
+					addressCount++;
+				}
+			}
+		}
+		sensors.requestTemperatures();
+
+		for (auto i = 0; i < addressCount; i++) {
+			devices[i].val = sensors.getTempC(devices[i].addr);
+		}
+	}
+
+	void printAddr(DeviceAddress addr) {
+		const char *digits("0123456789ABCDEF");
+		for (auto i = 0; i < 8; i++) {
+			Serial.print(digits[addr[i] >> 4]);
+			Serial.print(digits[addr[i] & 15]);
+		}
+	}
+
+	void publish() {
+		for (auto i = 0; i < addressCount; i++) {
+			auto& addr = devices[i].addr;
+			auto val = devices[i].val;
+#ifdef DEBUG
+			printAddr(addr);
+			Serial.print("=");
+			Serial.print(val);
+			Serial.println("ºC");
+#endif
+			if (val == 85.0 || val == -127.0)
+				continue;
+			char buf[topicLen + 32];
+			sprintf(buf, "%s%02x%02x%02x%02x%02x%02x%02x%02x",
+				topic,
+				addr[0], addr[1], addr[2], addr[3], addr[4], addr[5], addr[6], addr[7]);
+			char msg[16];
+			snprintf(msg, sizeof(msg), "%.1f", val);
+
+			client->publish(buf, msg);
+		}
+	}
+
+	const char* topic;
+	uint8_t topicLen;
+
+	OneWire* oneWire;
+	DallasTemperature sensors;
+	PubSubClient* client;
+
+	static const uint8_t ADDRESS_MAX = 8;
+	uint8_t addressCount = 0;
+	struct Device {
+		DeviceAddress addr;
+		float val;
+	};
+	Device devices[ADDRESS_MAX];
+};
+
+} // namespace gemha
